09b93df5f6ce5629984157d0167dc504ab3be3f7
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2021-06-25 21:40:49 -0300

    Creating the question removal functionality

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -6,7 +6,10 @@ import { Question } from '../components/Question'
 
 import { useRoom } from '../hooks/useRoom'
 
+import { database } from '../services/firebase'
+
 import logoImg from '../assets/images/logo.svg'
+import deleteImg from '../assets/images/delete.svg'
 
 import '../styles/room.scss'
 
@@ -23,6 +26,12 @@ export function AdminRoom() {
   // quando uma lógica é compartilhada em mais
   // de um componente, podemos criar um hook
   const { questions, title } = useRoom(roomId)
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      const questionRef = await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+    }
+  }
 
   return (
     <div id="page-room">
@@ -49,7 +58,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             )
           })}
         </div>
