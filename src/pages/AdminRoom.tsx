5f9fd7ff81dff555cbca75fe9969be417c7da4d3
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2021-06-25 21:24:46 -0300

    Creation of like functionality

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,14 +1,10 @@
-import { useState, FormEvent } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode'
 import { Question } from '../components/Question'
 
-import { useAuth } from '../hooks/useAuth'
 import { useRoom } from '../hooks/useRoom'
-
-import { database } from '../services/firebase'
 
 import logoImg from '../assets/images/logo.svg'
 
@@ -20,7 +16,6 @@ type RoomParams = {
 
 export function AdminRoom() {
   const params = useParams<RoomParams>()
-  const { user } = useAuth()
 
   // pegando o id da rota, que está vindo na rota
   const roomId = params.id
@@ -28,39 +23,6 @@ export function AdminRoom() {
   // quando uma lógica é compartilhada em mais
   // de um componente, podemos criar um hook
   const { questions, title } = useRoom(roomId)
-
-  // pegando os parâmetros que são repassados na rota
-  const [newQuestion, setNewQuestion] = useState('')
-
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault()
-
-    if (newQuestion.trim() === '') {
-      return
-    }
-
-    if (!user) {
-      throw new Error('You must be logged in')
-    }
-
-    // pegando as informações da pergunta
-    // realizada pelo usuário
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user?.name,
-        avatar: user.avatar
-      },
-      isHighlighter: false,
-      isAnswer: false
-    }
-
-    // criando registro da pergunta no DB, indicando
-    // qual a sala através do "roomId"
-    await database.ref(`rooms/${roomId}/questions`).push(question)
-
-    setNewQuestion('')
-  }
 
   return (
     <div id="page-room">
